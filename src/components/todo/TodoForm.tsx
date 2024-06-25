06fabbbec5dae78f99b99a277a5708dfdefdfec1
AALabonya <1aklimakter@gmail.com>
2024-06-24 21:17:05 -0700

    added todo

--- src/components/todo/TodoForm.tsx
+++ src/components/todo/TodoForm.tsx
@@ -2,25 +2,18 @@ export default function TodoForm() {
   return (
     <div>
       <form className="p-5">
-        <label> Name:</label>
+        <label> ID:</label>
         <input
           type="text"
-          id="FirstName"
-          name="first_name"
+          id="id"
+          name="id"
           className=" mt-1 w-full  rounded-md border py-2 bg-white text-sm text-gray-700 shadow-sm"
         />
-        <label> Age:</label>
+        <label> Title:</label>
         <input
           type="text"
-          id="FirstName"
-          name="first_name"
-          className=" mt-1 w-full  rounded-md border py-2 bg-white text-sm text-gray-700 shadow-sm"
-        />
-        <label> Hobbies:</label>
-        <input
-          type="text"
-          id="FirstName"
-          name="first_name"
+          id="title"
+          name="title"
           className=" mt-1 w-full  rounded-md border py-2 bg-white text-sm text-gray-700 shadow-sm"
         />
         <button className="bg-green-300 w-full mt-2 py-2 font-semibold">
